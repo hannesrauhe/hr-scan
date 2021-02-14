1e11dc28ac4d1ed7e11631dd94cc6a8942866906
Hannes Rauhe <mail+github@rauhe.eu>
2021-02-14 18:16:50 +0100

    some notes to repeat this later

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hr-scan
-Script to scan and upload to nextcloud on the command line
+
+Event script for insaned (https://github.com/abusenius/insaned) to scan and upload PDF to nextcloud/webdav on the command line.
+
+# Setup
+
+Put the start script in you insaned event dir
+
+Check the notes in setup.sh for some pointers
--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-# won't do this automatically for now:
+## won't do this automatically for now:
 
 # sudo apt install sane imagemagick
-# sudo adduser $(whoami) scanner
-# udev something
+## remove the wild PDF policy
 # sudo nano /etc/ImageMagick-6/policy.xml
+
+## only if you want to be able to run scripts as non-root
+# sudo adduser $(whoami) scanner
 # lsusb
 # echo UBSYSTEM==\"usb\", ATTRS{idVendor}==\"04a9\", ATTRS{idProduct}==\"1608\", MODE=\"0666\" > /tmp/hrscan
 # sudo cp /tmp/hrscan  /etc/udev/rules.d/90-scanner.rules
-# restart
+## restart
+
+## insaned on raspberry pi
+# git clone https://github.com/abusenius/insaned.git
+# sudo apt install libsane-dev
+# cd insaned
+# make
+# sudo cp insaned /usr/bin
+# sudo cp systemd/insaned.service /etc/systemd/system
+# touch /etc/default/insaned
+# systemctl start insaned
+# systemctl enable insaned
+# copy this repo to /etc/insaned/events
