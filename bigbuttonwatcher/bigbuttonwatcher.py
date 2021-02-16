1e3984988e9f2b8dc638c15d80f40e61a342b2eb
Hannes Rauhe <mail+github@rauhe.eu>
2021-02-16 23:23:21 +0100

    reorganize

---
+++ bigbuttonwatcher/bigbuttonwatcher.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import usb,sys,time
+from enum import Enum
+
+idVendor = 0x1d34
+idProduct = 0x000d
+
+class ButtonState(Enum):
+  UNDEF=0
+  LID_DOWN=21
+  PRESSED=22
+  LID_UP=23
+
+def findButton():
+  for bus in usb.busses():
+    for dev in bus.devices:
+      if dev.idVendor == idVendor and dev.idProduct == idProduct:
+        return dev
+  return None
+
+def state_change(last,new):
+  if new==ButtonState.PRESSED:
+    print("Button pressed")
+  elif last==ButtonState.LID_DOWN and new==ButtonState.LID_UP:
+    print("Lid opened")
+  elif last==ButtonState.LID_UP and new==ButtonState.LID_DOWN:
+    print("Lid closed")
+
+def main():
+  dev = findButton()
+  if dev == None:
+    print("Cannot find panic button device")
+    sys.exit(1)
+
+  handle = dev.open()
+  interface = dev.configurations[0].interfaces[0][0]
+  endpoint = interface.endpoints[0]
+
+  try:
+    handle.detachKernelDriver(interface)
+  except Exception:
+    pass
+
+  handle.claimInterface(interface)
+
+  last_event = 0x0
+  while True:
+    result = handle.controlMsg(requestType=0x21,
+                              request= 0x09,
+                              value= 0x0200,
+                              buffer="\x00\x00\x00\x00\x00\x00\x00\x02")
+
+    try:
+      result = handle.interruptRead(endpoint.address, endpoint.maxPacketSize)
+      if result[0]!=last_event:
+        state_change(ButtonState(last_event), ButtonState(result[0]))
+        last_event = result[0]
+    except Exception:
+      # Sometimes this fails. Unsure why.
+      pass
+    time.sleep(endpoint.interval / float(1000))
+
+  handle.releaseInterface()
+
+if __name__ == "__main__":
+    main()
